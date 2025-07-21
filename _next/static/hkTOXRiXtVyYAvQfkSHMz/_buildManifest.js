a0838d6ee0b63678fd96483dab874eba362f97c7
Igor <samptv59@gmail.com>
2025-07-21 15:45:01 +0300

    Updates

---
+++ _next/static/hkTOXRiXtVyYAvQfkSHMz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1d8b5f446848c5f7.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
